a5aca9003ac3524448d61bfb4d115a9ff073ea39
Syrenny <117575087+Syrenny@users.noreply.github.com>
2023-02-10 15:37:01 +0700

    Update task1.c

--- task1.c
+++ task1.c
@@ -3,6 +3,8 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #define N 10000000
+#define PGI_ACC_TIME 1 
+
 
 void FuncArray(double** my_array, int len)
 {
@@ -27,12 +29,15 @@ double SumArray(double* my_array, int len)
 
 int main()
 {
-	double* my_array = (double*)malloc(sizeof(double));
-	long long len = N;
-	FuncArray(&my_array, len);
-	/*for (int i = 0; i < len; i++)
-		printf("%f ", my_array[i]);*/
-	//printf("%f", SumArray(my_array, len));
-	SumArray(my_array, len);
-	return 0;
+	#pragma acc kernels 
+	{
+		double* my_array = (double*)malloc(sizeof(double));
+		long long len = N;
+		FuncArray(&my_array, len);
+		/*for (int i = 0; i < len; i++)
+			printf("%f ", my_array[i]);*/
+			//printf("%f", SumArray(my_array, len));
+		SumArray(my_array, len);
+		return 0;
+	}
 }
