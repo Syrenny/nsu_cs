ace5646efc5e8c829cd11a8d834a68f3b23e3fb6
Syrenny <117575087+Syrenny@users.noreply.github.com>
2023-02-12 15:28:07 +0700

    Create task1.c

---
+++ task1.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define TYPE double
+#define _USE_MATH_DEFINES
+#include <math.h>
+#define N 10000000
+
+void FuncArray(TYPE** my_array, int len)
+{
+	TYPE* temp = (TYPE*)malloc(sizeof(TYPE) * len);
+	for (int i = 0; i < len; i++)
+	{
+		TYPE k = (TYPE)i / len;
+		temp[i] = sin(k * 2.0 * M_PI);
+	}
+	*my_array = temp;
+}
+
+TYPE SumArray(TYPE* my_array, int len)
+{
+	TYPE res = 0;
+	for (int i = 0; i < len; i++)
+		res += my_array[i];
+	return res;
+}
+
+int main()
+{
+	TYPE* my_array = (TYPE*)malloc(sizeof(TYPE));
+	long long len = N;
+	FuncArray(&my_array, len);
+	/*for (int i = 0; i < len; i++)
+		printf("%f ", my_array[i]);*/
+		//printf("%f", SumArray(my_array, len));
+	SumArray(my_array, len);
+	return 0;
+}
