66cf52b8f3d19e4bcfe6c7e310de8d50259f3b0e
Syrenny <117575087+Syrenny@users.noreply.github.com>
2023-02-12 15:05:54 +0700

    Update task1.c

--- task1.c
+++ task1.c
@@ -3,16 +3,18 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #define N 10000000
-#define PGI_ACC_TIME 1 
-
 
 void FuncArray(double** my_array, int len)
 {
 	double* temp = (double*)malloc(sizeof(double) * len);
-	for (int i = 0; i < len; i++)
-	{
-		double k = (double)i / len;
-		temp[i] = sin(k * 2.0 * M_PI);
+	#pragma acc parallel 
+	{	
+		#pragma acc parallel loop
+		for (int i = 0; i < len; i++)
+		{
+			double k = (double)i / len;
+			temp[i] = sin(k * 2.0 * M_PI);
+		}
 	}
 	*my_array = temp;
 }
@@ -20,24 +22,24 @@ void FuncArray(double** my_array, int len)
 double SumArray(double* my_array, int len)
 {
 	double res = 0;
-	for (int i = 0; i < len; i++)
+	#pragma acc parallel
 	{
-		res += my_array[i];
+		#pragma acc parallel loop reduction(+:res)
+		for (int i = 0; i < len; i++)
+			res += my_array[i];
 	}
+	
 	return res;
 }
 
 int main()
 {
-	#pragma acc kernels 
-	{
-		double* my_array = (double*)malloc(sizeof(double));
-		long long len = N;
-		FuncArray(&my_array, len);
-		/*for (int i = 0; i < len; i++)
-			printf("%f ", my_array[i]);*/
-			//printf("%f", SumArray(my_array, len));
-		SumArray(my_array, len);
-		return 0;
-	}
+	double* my_array = (double*)malloc(sizeof(double));
+	long long len = N;
+	FuncArray(&my_array, len);
+	/*for (int i = 0; i < len; i++)
+		printf("%f ", my_array[i]);*/
+		//printf("%f", SumArray(my_array, len));
+	SumArray(my_array, len);
+	return 0;
 }
