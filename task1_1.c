a91b8df29ab672c552daa59516b020ce70753ee6
Hoster <i.syrennyj@g.nsu.ru>
2023-02-14 09:08:38 +0000

    faster112us

--- task1_1.c
+++ task1_1.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define TYPE double
+#define TYPE float
 #define _USE_MATH_DEFINES
 #include <math.h>
 #define N 10000000
@@ -9,16 +9,16 @@ int main()
 {
 	TYPE* my_array = (TYPE*)malloc(sizeof(TYPE)* N);
 	TYPE res = 0;
-	#pragma acc create(my_array[:N]) copyin(N) copy(res)
+	#pragma acc data create(my_array[:N]) copyin(N) copy(res)
 	{
-		#pragma acc kernels
+		#pragma acc parallel loop vector vector_length(160) gang 
 		for (int i = 0; i < N; i++)
 		{
 			TYPE k = (TYPE)i / N;
 			my_array[i] = sin(k * 2.0 * M_PI);
 		}
 
-		#pragma acc kernels reduction(+:res)
+		#pragma acc parallel loop reduction(+:res) vector vector_length(160) gang
 		for (int i = 0; i < N; i++)
 			res += my_array[i];
 	}
