4d963a150de28a46a42028a4d19781efd210e59b
Syrenny <117575087+Syrenny@users.noreply.github.com>
2023-04-04 15:00:49 +0700

    Update run.sh

--- task2/run.sh
+++ task2/run.sh
@@ -1,17 +1,14 @@
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo CPU multicore computations
 pgc++ -ta=multicore -o main main.cpp 
 ./main -size 128
 ./main -size 256
 ./main -size 512
 ./main -size 1024
-echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 echo CPU host computations 
 pgc++ -ta=host -o main main.cpp
 ./main -size 128
 ./main -size 256
 ./main -size 512
-echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 echo GPU computations
 pgc++ -acc -o main main.cpp
 nsys profile --trace=openacc -o gpu_128 main -size 128 -max_iter 100
