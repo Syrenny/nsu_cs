3f051bf116e70e1853109b2aed487375da6e3b05
Syrenny <i.syrennyj@g.nsu.ru>
2023-04-08 04:36:55 +0000

    final

--- task2/main.cpp
+++ task2/main.cpp
@@ -25,7 +25,7 @@ double corners[4] = { 10, 20, 30, 20 };
 int main(int argc, char** argv) 
 {
     int size = 128, max_iter = 1000000;
-    double min_error = 10e-6;
+    double min_error = 1e-6;
     for (int i = 0; i < argc; i++)
     {
         if (std::string(argv[i]).find("-size") != std::string::npos)
@@ -42,6 +42,7 @@ int main(int argc, char** argv)
         }
     }
     std::cout << "Size = " << size << std::endl;
+
     auto* A = new double[size * size];
     auto* A_new = new double[size * size];
 
@@ -51,20 +52,15 @@ int main(int argc, char** argv)
     A[size - 1] = corners[1];
     A[size * size - 1] = corners[2];
     A[size * (size - 1)] = corners[3];
-
     int full_size = size * size;
     double step = (corners[1] - corners[0]) / (size - 1);
 
-#pragma acc enter data copyin(A[0:full_size]) create(A_new[0:full_size])
+    for (int i = 1; i < size - 1; i++) 
     {
-#pragma acc parallel loop seq gang num_gangs(size) vector vector_length(size)
-        for (int i = 1; i < size - 1; i++) 
-        {
-            A[i] = corners[0] + i * step;
-            A[i * size + (size - 1)] = corners[1] + i * step;
-            A[i * size] = corners[0] + i * step;
-            A[size * (size - 1) + i] = corners[3] + i * step;
-        }
+        A[i] = corners[0] + i * step;
+        A[i * size + (size - 1)] = corners[1] + i * step;
+        A[i * size] = corners[0] + i * step;
+        A[size * (size - 1) + i] = corners[3] + i * step;
     }
 
     std::memcpy(A_new, A, sizeof(double) * full_size);
@@ -72,35 +68,37 @@ int main(int argc, char** argv)
     double error = 1.0;
     int iter = 0;
     time_start();
-
-#pragma acc enter data copyin(A_new[0:full_size], A[0:full_size], error, iter, min_error, max_iter)
+    
+#pragma acc data copyin(A[0:full_size], A_new[0:full_size], error)
     while (error > min_error && iter < max_iter) 
     {
         iter++;
-#pragma kernels
-	    {
-	error = 0.0;
-	    }
-
+        if(iter % 100 == 0 || iter < 100)
+        {
+	        error = 0.0;
 #pragma acc update device(error)
-#pragma acc data present(A, A_new)
-#pragma acc parallel loop vector vector_length(300) gang num_gangs(300) reduction(max:error) async(1)
+        }
+#pragma acc data present(A, A_new, error)
+#pragma acc parallel loop independent collapse(2) vector vector_length(256) gang num_gangs(256) reduction(max:error)
         for (int i = 1; i < size - 1; i++)
         {
             for (int j = 1; j < size - 1; j++)
             {
                 A_new[i * size + j] = 0.25 * (A[i * size + j - 1] + A[(i - 1) * size + j] + A[(i + 1) * size + j] + A[i * size + j + 1]);
-                error = fmax(error, A_new[i * size + j] - A[i * size + j]);
+                error = fmax(error, fabs(A_new[i * size + j] - A[i * size + j]));
             }
         }
         double* temp = A;
         A = A_new;
         A_new = temp;
-    }
+        if(iter % 100 == 0 || iter < 100)
+        {
 #pragma acc update host(error)
+        }
+    }
     std::cout << "Computation time(ms): " << time_stop() << std::endl;
     std::cout << "Error: " << error << std::endl;
-    std::cout << "Iteration: " << iter << std::endl;
+    std::cout << "Iteration: " << iter << std::endl << std::endl;
     delete[]A;
     delete[]A_new;
     return 0;
--- task2/run.sh
+++ task2/run.sh
@@ -1,17 +1,33 @@
+rm reports.txt
+touch reports.txt
+echo CPU multicore computations >> reports.txt
 echo CPU multicore computations
+echo ... 
 pgc++ -ta=multicore -o main main.cpp 
-./main -size 128
-./main -size 256
-./main -size 512
-./main -size 1024
-echo CPU host computations 
+./main -size 128 >> reports.txt
+./main -size 256 >> reports.txt
+./main -size 512 >> reports.txt
+echo done.
+echo CPU host computations >> reports.txt
+echo CPU host computations
+echo ...
 pgc++ -ta=host -o main main.cpp
-./main -size 128
-./main -size 256
-./main -size 512
+./main -size 128 >> reports.txt
+./main -size 256 >> reports.txt
+./main -size 512 >> reports.txt
+echo done.
+rm *.nsys-rep
+echo GPU computations >> reports.txt
 echo GPU computations
+echo ...
 pgc++ -acc -o main main.cpp
-nsys profile --trace=openacc -o gpu_128 main -size 128 -max_iter 100
-nsys profile --trace=openacc -o gpu_256 main -size 256 -max_iter 100
-nsys profile --trace=openacc -o gpu_512 main -size 512 -max_iter 100
-nsys profile --trace=openacc -o gpu_1024 main -size 1024 -max_iter 100
+./main -size 128 -max_iter 1000000 >> reports.txt
+./main -size 256 -max_iter 1000000 >> reports.txt
+./main -size 512 -max_iter 1000000 >> reports.txt
+./main -size 1024 -max_iter 1000000 >> reports.txt
+nsys profile --trace=openacc,nvtx -o gpu_128 main -size 128 -max_iter 50 >> reports.txt
+nsys profile --trace=openacc,nvtx -o gpu_256 main -size 256 -max_iter 50 >> reports.txt
+nsys profile --trace=openacc,nvtx -o gpu_512 main -size 512 -max_iter 50 >> reports.txt
+nsys profile --trace=openacc,nvtx -o gpu_1024 main -size 1024 -max_iter 50 >> reports.txt
+echo done.
+echo end.
