06162336b5029f9d8bf1d3a01963ee177ef6f889
Syrenny <117575087+Syrenny@users.noreply.github.com>
2023-05-15 14:47:07 +0700

    prefinal
    - upgrade of memory freeing 
    - using pinned memory 
    - std::swap removed 
    - __global__ functions calculate over two-dim. arrays
    - handling of invalid parameters added

--- task4/main_cudaGraph.cu
+++ task4/main_cudaGraph.cu
@@ -9,15 +9,34 @@
 // Период вычисления ошибки
 #define ERROR_COMPUTATION_STEP 100
 
+#define CUDA_DEBUG
+
+#ifdef CUDA_DEBUG
+
+#define CUDA_CHECK_ERROR(err)           \
+if (err != cudaSuccess) {          \
+printf("Cuda error: %s\n", cudaGetErrorString(err));    \
+printf("Error in file: %s, line: %i\n", __FILE__, __LINE__);  \
+my_cudaFree(8, error, A, A_new, device_A, device_A_new, device_error, device_error_matrix, temp_stor);	\
+exit(-1);	\
+}                 \
+
+#else
+
+#define CUDA_CHECK_ERROR(err)
+
+#endif
 
 // __global__ - функция вызывается с CPU или GPU и выполняется на GPU
 // Главная функция - расчёт поля 
 __global__ void calculate_new_matrix(double* A, double* A_new, size_t size)
 {
-	size_t i = blockIdx.x;
-	size_t j = threadIdx.x;
-	// Проверяем, чтобы не выйти в ходе алгоритма за границы памяти, да и граничные условия пересчитывать не нужно	
-	if(!(blockIdx.x == 0 || threadIdx.x == 0 || blockIdx.x > size - 1 || threadIdx.x > size - 1))
+	unsigned int j = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int i = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (i * size + j > size * size) return;
+	// Проверка, чтобы не выходить за границы массива и не пересчитывать граничные условия	
+	if(!(i == 0 || j == 0 || i >= size - 1 || j >= size - 1))
 	{
 		A_new[i * size + j] = 0.25 * (A[i * size + j - 1] + A[(i - 1) * size + j] +
 							A[(i + 1) * size + j] + A[i * size + j + 1]);		
@@ -27,9 +46,11 @@ __global__ void calculate_new_matrix(double* A, double* A_new, size_t size)
 // Функция рассчета матрицы ошибок
 __global__ void calculate_device_error_matrix(double* A, double* A_new, double* output_matrix, size_t size)
 {
-	size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-	// Проверяем, чтобы не выйти в ходе алгоритма за границы памяти, да и граничные условия пересчитывать не нужно
-	if(!((blockIdx.x == 0 || threadIdx.x == 0 || blockIdx.x > size - 1 || threadIdx.x > size - 1))
+	unsigned int j = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int i = blockIdx.y * blockDim.y + threadIdx.y;
+
+	size_t idx = i * size + j;
+	if(!(j == 0 || i == 0 || j == size - 1))
 	{
 		output_matrix[idx] = std::abs(A_new[idx] - A[idx]);
 	}
@@ -37,9 +58,9 @@ __global__ void calculate_device_error_matrix(double* A, double* A_new, double* 
 // Функция для вывода содержимого матрицы 
 void print_matrix(double* mx, int n, int m)
 {
-    for(int i=0; i<m; i++)
+    for(int i = 0; i < m; i++)
     {
-        for(int j=0; j<n; j++)
+        for(int j = 0; j < n; j++)
         {
             std::cout << mx[n * i + j] << " ";
         }
@@ -53,7 +74,11 @@ void my_cudaFree(int num, double* set...)
 	va_start(args, set);
 	for(int i = 0; i < num; i++)
 	{
-		cudaFree(va_arg(args, double*));
+		double* ptr = va_arg(args, double*);
+		if(ptr)
+		{
+			cudaFree(ptr);
+		}
 	}
 	va_end(args);
 }
@@ -62,6 +87,7 @@ double corners[4] = { 10, 20, 30, 20 };
 
 int main(int argc, char** argv)
 {
+	double *error, *A, *A_new, *device_A, *device_A_new, *device_error, *device_error_matrix, *temp_stor = NULL;
 	// Получаем значения из командной строки
 	int size = 128, max_iter = 1000000;
     double min_error = 1e-6;
@@ -73,7 +99,7 @@ int main(int argc, char** argv)
 			if(size < 0)
 			{
 				std::cout << "Invalid -size parameter" << std::endl;
-				return -1;
+				exit(-1);
 			}
         }
         else if (std::string(argv[i]).find("-max_iter") != std::string::npos)
@@ -82,6 +108,7 @@ int main(int argc, char** argv)
 			if(max_iter < 0)
 			{
 				std::cout << "Invalid -max_iter parameter" << std::endl;
+				exit(-1);
 			}
         }
         else if (std::string(argv[i]).find("-min_error") != std::string::npos)
@@ -90,16 +117,16 @@ int main(int argc, char** argv)
 			if(min_error <= 0)
 			{
 				std::cout << "Invalid -min_error parameter" << std::endl;
+				exit(-1);
 			}
         }
     }
     std::cout << "Size = " << size << std::endl;
 	
     // Выделение памяти на хосте
-    auto* A = new double[size * size];
-    auto* A_new = new double[size * size];
     int full_size = size * size;
-	
+	CUDA_CHECK_ERROR(cudaMallocHost(&A_new, full_size * sizeof(double)));
+	CUDA_CHECK_ERROR(cudaMallocHost(&A, full_size * sizeof(double)));
 	std::memset(A, 0, full_size * sizeof(double));
 
 	// Заполнение граничных условий
@@ -121,94 +148,31 @@ int main(int argc, char** argv)
 	std::memcpy(A_new, A, full_size * sizeof(double));
 
 	// Выбор устройства
-	cudaSetDevice(3);
+	cudaSetDevice(1);
 
 	// Выделяем память на девайсе и копируем память с хоста
-	double* device_A, *device_A_new, *device_error, *device_error_matrix, *temp_stor = NULL;
 	size_t temp_stor_size = 0;
 
-	cudaError_t cuda_status = cudaMalloc(&device_A, sizeof(double) * full_size);
-	if(cuda_status != cudaSuccess)
-	{
-		std::cout << "device_A allocation error" << std::endl;
-		delete[]A;
-		delete[]A_new;
-		return -1;
-	}
-
-	cuda_status = cudaMalloc(&device_A_new, sizeof(double) * full_size);
-	if(cuda_status != cudaSuccess)
-	{
-		std::cout << "device_A_new allocation error" << std::endl;
-		delete[]A;
-		delete[]A_new;
-		cudaFree(device_A);
-		return -1;
-	}
-
-	cuda_status = cudaMalloc(&device_error, sizeof(double));
-	if(cuda_status != cudaSuccess)
-	{
-		std::cout << "device_error allocation error" << std::endl;
-		delete[]A;
-		delete[]A_new;
-		my_cudaFree(2, device_A, device_A_new);
-		return -1;
-	}
-
-	cuda_status = cudaMalloc(&device_error_matrix, sizeof(double) * full_size); 
-	if(cuda_status != cudaSuccess)
-	{
-		std::cout << "device_error_matrix allocation error" << std::endl;
-		delete[]A;
-		delete[]A_new;
-		my_cudaFree(3, device_error, device_A, device_A_new);
-		return -1;
-	}
-
-	cuda_status = cudaMemcpy(device_A, A, sizeof(double) * full_size, cudaMemcpyHostToDevice);
-	if(cuda_status != cudaSuccess)
-	{
-		std::cout << "device_A copying error" << std::endl;
-		delete[]A;
-		delete[]A_new;
-		my_cudaFree(4, device_error_matrix, device_error, device_A, device_A_new);
-		return -1;
-	}
-
-	cuda_status = cudaMemcpy(device_A_new, A_new, sizeof(double) * full_size, cudaMemcpyHostToDevice);
-	if(cuda_status != cudaSuccess)
-	{
-		std::cout << "device_A_new copying error" << std::endl;
-		delete[]A;
-		delete[]A_new;
-		my_cudaFree(4, device_error_matrix, device_error, device_A, device_A_new);
-		return -1;
-	}
-	
+	CUDA_CHECK_ERROR(cudaMalloc(&device_A, sizeof(double) * full_size));
+	CUDA_CHECK_ERROR(cudaMalloc(&device_A_new, sizeof(double) * full_size));
+	CUDA_CHECK_ERROR(cudaMalloc(&device_error, sizeof(double)));
+	CUDA_CHECK_ERROR(cudaMalloc(&device_error_matrix, sizeof(double) * full_size));
+	CUDA_CHECK_ERROR(cudaMemcpy(device_A, A, sizeof(double) * full_size, cudaMemcpyHostToDevice));
+	CUDA_CHECK_ERROR(cudaMemcpy(device_A_new, A_new, sizeof(double) * full_size, cudaMemcpyHostToDevice));	
 
 	// Функция проверяет temp_stor==NULL, получаем размер временного буфера для редукции
 	cub::DeviceReduce::Max(temp_stor, temp_stor_size, device_error_matrix, device_error, full_size);
 	
 	// Выделяем память для буфера
-	cuda_status = cudaMalloc(&temp_stor, temp_stor_size);
-    if (cuda_status != cudaSuccess)
-    {
-        std::cout << "temp_stor allocation error" << std::endl;
-		delete[]A;
-		delete[]A_new;
-		my_cudaFree(4, device_error_matrix, device_error, device_A, device_A_new);
-		return -1;
-    }
+	CUDA_CHECK_ERROR(cudaMalloc(&temp_stor, temp_stor_size));
+
 	int iter = 0; 
-	double* error;
-	cudaMallocHost(&error, sizeof(double));
+	CUDA_CHECK_ERROR(cudaMallocHost(&error, sizeof(double)));
 	*error = 1.0;
 
-	bool isGraphCreated = false;
-	cudaStream_t stream, memoryStream;
+	bool is_graph_inited = false;
+	cudaStream_t stream;
 	cudaStreamCreate(&stream);
-	cudaStreamCreate(&memoryStream);
 	cudaGraph_t graph;
 	cudaGraphExec_t instance;
 
@@ -224,12 +188,11 @@ int main(int argc, char** argv)
 	while(iter < max_iter && *error > min_error)
 	{
 		// Расчет матрицы
-		if (isGraphCreated)
+		if (is_graph_inited)
 		{
 			cudaGraphLaunch(instance, stream);
 			
 			cudaMemcpyAsync(error, device_error, sizeof(double), cudaMemcpyDeviceToHost, stream);
-
 			cudaStreamSynchronize(stream);
 
 			iter += ERROR_COMPUTATION_STEP;
@@ -243,28 +206,21 @@ int main(int argc, char** argv)
 				calculate_new_matrix<<<gridDim, blockDim, 0, stream>>>(device_A_new, device_A, size);
 			}
 			// Расчитываем ошибку каждую сотую итерацию
-			calculate_device_error_matrix<<<threads * blocks * blocks, threads,  0, stream>>>(device_A, device_A_new, device_error_matrix, size);
+			calculate_device_error_matrix<<<gridDim, blockDim, 0, stream>>>(device_A, device_A_new, device_error_matrix, size);
 			cub::DeviceReduce::Max(temp_stor, temp_stor_size, device_error_matrix, device_error, full_size, stream);
 	
 			cudaStreamEndCapture(stream, &graph);
 			cudaGraphInstantiate(&instance, graph, NULL, NULL, 0);
-			isGraphCreated = true;
+			is_graph_inited = true;
   		}
 	}
     nvtxRangePop();
 	clock_t end = clock();
     std::cout << "Computation time(s): " << 1.0 * (end - start) / CLOCKS_PER_SEC << std::endl;
-    std::cout << "Error: " << error << std::endl;
+    std::cout << "Error: " << *error << std::endl;
     std::cout << "Iteration: " << iter << std::endl << std::endl;
 
 	// Освобождение памяти 
-	cudaFree(device_A);
-	cudaFree(device_A_new);
-	cudaFree(device_error_matrix);
-	cudaFree(temp_stor);
-	cudaFree(A);
-	cudaFree(A_new);
-	delete[]A;
-	delete[]A_new;
+	my_cudaFree(8, error, A, A_new, device_A, device_A_new, device_error, device_error_matrix, temp_stor);
 	return 0;
 }
